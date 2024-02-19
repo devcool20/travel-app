b155444b812bd003b100379da7880026b5e4b908
Divyanshu Sharma <113442152+devcool20@users.noreply.github.com>
2024-02-19 09:30:48 +0530

    mc1

--- components/Camp.tsx
+++ components/Camp.tsx
@@ -53,15 +53,15 @@ const Camp = () => {
       <div className="hide-scrollbar flex h-[340px] w-full items-start justify-start gap-8 overflow-x-auto lg:h-[400px] xl:h-[640px]">
         <CampSite 
           backgroundImage="bg-bg-img-1"
-          title="Putuk Truno Camp"
-          subtitle="Prigen, Pasuruan"
+          title="Manali Camp"
+          subtitle="Bharat"
           peopleJoined="50+ Joined"
         />
         <CampSite 
           backgroundImage="bg-bg-img-2"
           title="Mountain View Camp"
           subtitle="Somewhere in the Wilderness"
-          peopleJoined="50+ Joined"
+          peopleJoined="69+ Joined"
         />
       </div>
 
